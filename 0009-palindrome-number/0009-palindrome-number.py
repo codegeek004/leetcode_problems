2b2e970018310fbf4fbe6ffd2eaa419a71907d8e
Yash Vaishnav <121330128+codegeek004@users.noreply.github.com>
2024-11-23 17:05:53 +0530

    Time: 12 ms (28.7%), Space: 16.8 MB (9.62%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -3,9 +3,7 @@ class Solution:
         temp = x
         reverse = 0
         while(x>0):
-            #returns the last digit of the number
             digit = x%10
-            #for example if the reverse is 121 so here reverse is 0 by definition so 0*10 + digit is 1. It will left shift from the x//10 and it will be until it reaches 0.
             reverse = reverse*10 + digit
             x = x//10
         if temp == reverse:
