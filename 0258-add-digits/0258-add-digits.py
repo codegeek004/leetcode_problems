476f05870442e18b05fa91f4547c56903f4f150c
Yash Vaishnav <121330128+codegeek004@users.noreply.github.com>
2024-11-30 23:01:20 +0530

    Time: 0 ms (100%), Space: 12 MB (5.03%) - LeetHub

--- 0258-add-digits/0258-add-digits.py
+++ 0258-add-digits/0258-add-digits.py
@@ -5,18 +5,7 @@ class Solution(object):
         :type num: int
         :rtype: int
         """
-        # value = 0
-        # while(num!=0):
-        #     digit=num%10
-        #     value = digit+value
-        #     num = num//10
-        # value1 = 0
-        # print(value)
-        # if value>9:
-        #     value1=value%10+value//10
-        #     print(value1)
-        #     return value1
-        # return value
+
         while(num>9):
             num=num%10+num//10
         return num
